89fbe3fd325e20188475965075ef2a15052fe800
SuhasNale <juniornale1215@gmail.com>
2016-08-22 11:22:48 +0530

    login and logout functionality added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,22 @@
 angular.module('app.controllers', [])
 
-.controller('loginCtrl', function ($scope){
-	
+.controller('loginCtrl', function ($scope , $ionicPopup, $state){
+	$scope.login = function(data){
+	if (data.username == "admin" && data.password == "admin123") 
+	{
+		var alertPopup = $ionicPopup.alert({
+				title: 'Login',
+				template: 'Logged in successfully!'
+			});	
+		$state.go('welcome');
+	}
+	else{
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login Error!',
+        template: "Credentials Incorrect"
+      });
+    }
+};
 })
 	
 
